f7dcdc4afeb1682152461190af7b082feb0bc05f
Prayaan1711 <spyfyi1711@gmail.com>
2025-04-06 09:59:16 +0530

    1234

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+name = "Prayaan"
+age = 13
+weight = 42.5
+height = 5
+
+print("\nAfter typecasting...")
+age = str(age)
+print(age)
+print("Data type of age is ", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data type of weight is ", type(weight))
+
+height = int(height)
+print(height)
+print("Data type of height is ", type(height))

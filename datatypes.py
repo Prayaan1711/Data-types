556e4b128a103ae14a39d820a2b50e1257064454
Prayaan1711 <spyfyi1711@gmail.com>
2025-04-06 09:43:50 +0530

    1234

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+a = 17
+print("The type of a is:", type(a))
+
+b = 5.5
+print("The type of b is:", type(b))
+
+c = "coding"
+print("The type of c is:", type(c))
+
+d = True
+print("The type of d is:", type(d))

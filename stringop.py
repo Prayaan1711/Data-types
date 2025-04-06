6121c0f05d8aa55367b1ffc167200f35dbb17362
Prayaan1711 <spyfyi1711@gmail.com>
2025-04-06 10:20:36 +0530

    1234

---
+++ stringop.py
@@ -0,0 +1,22 @@
+str1 = "Hello"
+str2 = "World"
+print('str1 + str2 = ', str1 + str2)
+
+str = "string"
+print(str[::3])
+print(str[0::1])
+print(str[::-1])
+
+msg = "python is fun"
+print(msg.upper())
+
+text = input("Enter a string ")
+
+revtext = text[::-1]
+text = revtext
+
+print("Reverse of given string is ", text)
+
+name = input("Enter your name")
+surname = input("Enter your surname")
+print('Your full name is ', name + " " + surname)
